e2688c58b1eb6be2ddfe5821ddaeb832fa180296
Brendan8c <brendan8c@gmail.com>
2023-07-14 22:42:25 +0300

    Updated files

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,10 @@
-alert('Hello owl 2')
+alert('Hello owl 1');
 
 window.addEventListener('load', function0);
 
 function fixWay() {
-    let str = document.body.indexOf('Lorem')
-    str.replace('Lorem', 'Happy owl')
+  let str = document.body.indexOf('Lorem');
+  str.replace('Lorem', 'Happy owl');
 }
 
-fixWay()
+fixWay();
